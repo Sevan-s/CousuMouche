ef86089e59a8483af7d8101d7f1f7ff117988a70
Sevan-s <sevan.sarikaya@gmail.com>
2025-11-03 15:20:11 +0100

    [ADD and DEL] video

--- cousumouche_frontend/src/components/welcomeBanner.tsx
+++ cousumouche_frontend/src/components/welcomeBanner.tsx
@@ -1,24 +1,24 @@
 export default function WelcomeBanner() {
   return (
-    // <div className="relative w-full h-full overflow-hidden">
-    //   <video
-    //     src={"/videos/newIntro.mp4"}
-    //     autoPlay
-    //     loop
-    //     muted
-    //     playsInline
-    //     className="absolute top-0 left-1/2 h-full -translate-x-1/2 object-cover rounded-xl"
-    //   />
-    // </div>
     <div className="relative w-full h-full overflow-hidden">
       <video
-        src={"/videos/videoCm.mp4"}
+        src={"/videos/cmVideo.mp4"}
         autoPlay
         loop
         muted
         playsInline
-        className="absolute top-0 left-1/2 xl:w-5/12 lg:w-4/12 h-full -translate-x-1/2 object-cover rounded-xl object-[0%_72%] "
+        className="absolute top-0 left-1/2 h-full -translate-x-1/2 object-cover rounded-xl"
       />
     </div>
+    // <div className="relative w-full h-full overflow-hidden">
+    //   <video
+    //     src={"/videos/cmVideo.mp4"}
+    //     autoPlay
+    //     loop
+    //     muted
+    //     playsInline
+    //     className="absolute top-0 left-1/2 xl:w-5/12 lg:w-4/12 h-full -translate-x-1/2 object-cover rounded-xl object-[0%_72%] "
+    //   />
+    // </div>
   );
 }
